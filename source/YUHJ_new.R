72a16942a9f3ca8813e21389c10ed753a0d028bc
hoduni <99299546+hoduni@users.noreply.github.com>
2022-02-24 14:10:21 +0900

    Add files via upload

---
+++ source/YUHJ_new.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(shiny)
+library(ggplot2)
+library(gapminder)
+
+pd <- read.csv(file = 'C:/Users/wpghf/sta/source/adfee.csv')
+
+str(pd)
+
+ui <- fluidPage(titlePanel('주류별 광고비 지출'),
+    sidebarPanel(
+        sliderInput(inputId = 'year', label = '연도 :', min = 2010, max = 2019, value = 2010)),
+    
+    mainPanel(
+        plotOutput('distPlot'))
+        
+)
+
+server <- function (input, output) {
+    output$distPlot <- renderPlot({
+        userselection <- pd[, c(input$year - 2008)]
+        ggplot(data = pd, aes(x = reorder(what, userselection), y = userselection)) +
+            geom_bar(stat = 'identity', fill="antiquewhite3") + coord_flip() +
+            theme_bw() +
+            theme(panel.grid.major.x = element_blank()) +
+            labs(title = '주류별 광고비 지출액', x = '주류 종류', y = ' ') +
+            theme(plot.title = element_text(size = 24,
+                                            face = 'bold',
+                                            color = 'black'),
+                  axis.text.y = element_text(face = 'bold', size = 11))
+    })
+}
+
+
+shinyApp(ui, server)
+
